b368086335c273ccbfe7b1cf56298192d3710886
Kilian Jakstis <kjj6427>
2023-01-21 22:17:52 -0500

    finished functionality and GUI fixes

--- calc.py
+++ calc.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 class Calculator:
-    # hi
+
     def __init__(self):
         """
 
@@ -11,13 +11,15 @@ class Calculator:
         self.display = None
         self.window = tk.Tk()
         self.window.title("Calculator")
+        self.window.configure(bg="black")
         self.calc = tk.Frame(self.window, bg="black")
         self.prev_contents = ""
         self.display_contents = ""
         self.holder = 0
         # window setup
-        self.window.minsize(150, 275)
-        self.window.geometry("300x450")
+        self.window.minsize(340, 460)
+        self.window.maxsize(340, 460)
+        # self.window.geometry("300x450")
         self.calc.columnconfigure(4)
         self.calc.rowconfigure(8)
         self.calc.pack()
@@ -35,7 +37,7 @@ class Calculator:
             self.update()
 
     def numeric(self, z):
-        if len(self.display_contents) < 12:
+        if len(self.display_contents) < 13:
             self.display_contents = self.display_contents + str(z)
         self.update()
         return
@@ -51,17 +53,51 @@ class Calculator:
             self.update()
             return
 
-    def add(self):
-        pass
+    def equals(self):
+        if self.prev_contents != "" and self.display_contents != "":
+            y = float(self.holder)
+            z = float(self.display_contents)
+            o = self.prev_contents[len(self.prev_contents) - 1]
+            if o == '+':
+                self.display_contents = str(float(y + z))
+            if o == '-':
+                self.display_contents = str(float(y - z))
+            if o == '*':
+                self.display_contents = str(float(y * z))
+            if o == '/':
+                self.display_contents = str(float(y / z))
+            self.holder = None
+            self.prev_contents = ""
+            self.update()
+        if self.prev_contents != "" and self.display_contents == "":
+            self.prev_contents = ""
+            self.display_contents = str(self.holder)
+            self.holder = None
+            self.update()
 
-    def sub(self):
-        pass
-
-    def mul(self):
-        pass
-
-    def div(self):
-        pass
+    def operation(self, o):
+        if self.display_contents != "":
+            if self.prev_contents == "":
+                self.holder = float(self.display_contents)
+                self.prev_contents = str(self.holder) + str(o)
+                self.display_contents = ""
+                self.update()
+            else:
+                if o == '+':
+                    self.holder += float(self.display_contents)
+                if o == '-':
+                    self.holder -= float(self.display_contents)
+                if o == '*':
+                    self.holder *= float(self.display_contents)
+                if o == '/':
+                    self.holder /= float(self.display_contents)
+                self.prev_contents = str(self.holder) + str(o)
+                self.display_contents = ""
+                self.update()
+        else:
+            if self.prev_contents != "":
+                self.prev_contents = self.prev_contents[:len(self.prev_contents) - 1] + str(o)
+                self.update()
 
     def inputs(self, arg):
         if arg == '.':
@@ -70,30 +106,23 @@ class Calculator:
         if arg.isnumeric():
             self.numeric(arg)
             return
-        if arg == 'e':
+        if arg == 'c':
             self.display_contents = ""
             self.prev_contents = ""
+            self.holder = None
             self.update()
             return
-        if arg == 'c':
+        if arg == 'e':
             self.display_contents = ""
             self.update()
             return
         if arg == '<':
             self.back_space()
             return
-        if arg == '+':
-            self.add()
-            return
-        if arg == '-':
-            self.sub()
-            return
-        if arg == '*':
-            self.mul()
-            return
-        if arg == '/':
-            self.div()
-            return
+        if arg == '=':
+            self.equals()
+        if arg == '+' or arg == '-' or arg == '*' or arg == '/':
+            self.operation(arg)
         if arg == '#':
             if len(self.display_contents) >= 1 and self.display_contents != ".":
                 if "." in self.display_contents:
@@ -104,28 +133,29 @@ class Calculator:
                 return
 
     def make_calc(self, w):
-        button1 = tk.Button(w, text="1", width=5, command=lambda: self.inputs('1'), height=2)
-        button2 = tk.Button(w, text="2", width=5, command=lambda: self.inputs('2'), height=2)
-        button3 = tk.Button(w, text="3", width=5, command=lambda: self.inputs('3'), height=2)
-        button4 = tk.Button(w, text="4", width=5, command=lambda: self.inputs('4'), height=2)
-        button5 = tk.Button(w, text="5", width=5, command=lambda: self.inputs('5'), height=2)
-        button6 = tk.Button(w, text="6", width=5, command=lambda: self.inputs('6'), height=2)
-        button7 = tk.Button(w, text="7", width=5, command=lambda: self.inputs('7'), height=2)
-        button8 = tk.Button(w, text="8", width=5, command=lambda: self.inputs('8'), height=2)
-        button9 = tk.Button(w, text="9", width=5, command=lambda: self.inputs('9'), height=2)
-        button0 = tk.Button(w, text="0", width=5, command=lambda: self.inputs('0'), height=2)
-        button_negate = tk.Button(w, text="+/-", width=5, command=lambda: self.inputs('#'), height=2)
-        button_dot = tk.Button(w, text=".", width=5, command=lambda: self.inputs('.'), height=2)
-        button_add = tk.Button(w, text="+", width=5, command=lambda: self.inputs('+'), height=2)
-        button_sub = tk.Button(w, text="-", width=5, command=lambda: self.inputs('-'), height=2)
-        button_mul = tk.Button(w, text="*", width=5, command=lambda: self.inputs('*'), height=2)
-        button_div = tk.Button(w, text="/", width=5, command=lambda: self.inputs('/'), height=2)
-        button_enter = tk.Button(w, text="=", width=5, command=lambda: self.inputs('='), height=2)
-        button_del = tk.Button(w, text="<<", width=5, command=lambda: self.inputs('<'), height=2)
-        button_ca = tk.Button(w, text="CE", width=5, command=lambda: self.inputs('e'), height=2)
-        button_c = tk.Button(w, text="C", width=5, command=lambda: self.inputs('c'), height=2)
-        display = tk.Label(w, text="Welcome", width=25, height=5, bg="light grey")
-        prev = tk.Label(w, width=5, bg="light grey")
+        button1 = tk.Button(w, text="1", width=10, command=lambda: self.inputs('1'), height=4, relief="raised")
+        button2 = tk.Button(w, text="2", width=10, command=lambda: self.inputs('2'), height=4, relief="raised")
+        button3 = tk.Button(w, text="3", width=10, command=lambda: self.inputs('3'), height=4, relief="raised")
+        button4 = tk.Button(w, text="4", width=10, command=lambda: self.inputs('4'), height=4, relief="raised")
+        button5 = tk.Button(w, text="5", width=10, command=lambda: self.inputs('5'), height=4, relief="raised")
+        button6 = tk.Button(w, text="6", width=10, command=lambda: self.inputs('6'), height=4, relief="raised")
+        button7 = tk.Button(w, text="7", width=10, command=lambda: self.inputs('7'), height=4, relief="raised")
+        button8 = tk.Button(w, text="8", width=10, command=lambda: self.inputs('8'), height=4, relief="raised")
+        button9 = tk.Button(w, text="9", width=10, command=lambda: self.inputs('9'), height=4, relief="raised")
+        button0 = tk.Button(w, text="0", width=10, command=lambda: self.inputs('0'), height=4, relief="raised")
+        button_negate = tk.Button(w, text="+/-", width=10, command=lambda: self.inputs('#'), height=4, relief="raised")
+        button_dot = tk.Button(w, text=".", width=10, command=lambda: self.inputs('.'), height=4, relief="raised")
+        button_add = tk.Button(w, text="+", width=10, command=lambda: self.inputs('+'), height=4, relief="raised")
+        button_sub = tk.Button(w, text="-", width=10, command=lambda: self.inputs('-'), height=4, relief="raised")
+        button_mul = tk.Button(w, text="*", width=10, command=lambda: self.inputs('*'), height=4, relief="raised")
+        button_div = tk.Button(w, text="/", width=10, command=lambda: self.inputs('/'), height=4, relief="raised")
+        button_enter = tk.Button(w, text="=", width=10, command=lambda: self.inputs('='), height=4, relief="raised")
+        button_del = tk.Button(w, text="<<", width=10, command=lambda: self.inputs('<'), height=4, relief="raised")
+        button_ca = tk.Button(w, text="CE", width=10, command=lambda: self.inputs('e'), height=4, relief="raised")
+        button_c = tk.Button(w, text="C", width=10, command=lambda: self.inputs('c'), height=4, relief="raised")
+        display = tk.Label(w, text="Welcome", width=35, height=5, bg="light grey", font=("Arial", 13),
+                           borderwidth=1, relief="solid")
+        prev = tk.Label(w, width=15, bg="light grey", font=("Arial", 10))
         button1.grid(row=6, column=0)
         button2.grid(row=6, column=1)
         button3.grid(row=6, column=2)
